cfd98c3a482018ead7a324346d0fb3dbd0ce6618
wanfeng-win <804121985@qq.com>
2025-09-26 19:55:36 +0800

    init

---
+++ premake5.lua
@@ -0,0 +1,45 @@
+include "./vendor/premake/premake_customization/solution_items.lua"
+include "Dependencies.lua"
+
+workspace "Hazel"
+	architecture "x86_64"
+	startproject "Hazelnut"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	solution_items
+	{
+		".editorconfig"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+group "Dependencies"
+	include "vendor/premake"
+	include "Hazel/vendor/Box2D"
+	include "Hazel/vendor/GLFW"
+	include "Hazel/vendor/Glad"
+	include "Hazel/vendor/msdf-atlas-gen"
+	include "Hazel/vendor/imgui"
+	include "Hazel/vendor/yaml-cpp"
+
+group ""
+
+group "Misc"
+	include "Sandbox"
+	include "Hazel-ScriptCore"
+group ""
+
+
+include "Hazel"
+include "Hazelnut"
